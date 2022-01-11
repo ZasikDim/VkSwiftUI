700bc6977bdeae756bea56e799736f2b335b9076
Zasik <mitya.leonov@gmail.com>
2022-01-11 14:13:47 +0100

    Сверстал экран login

---
+++ VK-SwiftUI/VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUIApp.swift
+//  VK-SwiftUI
+//
+//  Created by Dmitry Zasenko on 10.01.22.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
